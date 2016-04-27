60b107f8144417d012324ee7ecb8f8d0b52d75ab
marisachao <marisa.chao@wustl.edu>
2016-04-27 18:07:44 -0500

    Update student-report.js

--- js/student-report.js
+++ js/student-report.js
@@ -1,1 +1,13 @@
-alert("testing");
+var message = '';
+var student;
+
+function(message) {
+  document.write("<section>" + message + "</section>");
+}
+
+for (var i = 0; i<students.length; i++) {
+  student = students[i];
+  message += '<h1>' + student.name + '</h1>;
+}
+
+print(message);
